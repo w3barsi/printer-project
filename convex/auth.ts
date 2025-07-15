6ba09b61772194c8fda3270a14d91eba8101b128
w3barsi <darcybalaga@gmail.com>
2025-07-15 14:38:14 +0800

    fixed publicAuthFunction missing a component

--- convex/auth.ts
+++ convex/auth.ts
@@ -9,25 +9,32 @@ import type { DataModel, Id } from "./_generated/dataModel"
 
 // Typesafe way to pass Convex functions defined in this file
 const authFunctions: AuthFunctions = internal.auth
+const publicAuthFunctions: PublicAuthFunctions = api.auth
 
 // Initialize the component
 export const betterAuthComponent = new BetterAuth(components.betterAuth, {
 	authFunctions,
+	publicAuthFunctions,
 })
 
 // These are required named exports
-export const { createUser, updateUser, deleteUser, createSession } =
-	betterAuthComponent.createAuthFunctions<DataModel>({
-		// Must create a user and return the user id
-		onCreateUser: async (ctx, user) => {
-			return ctx.db.insert("users", {})
-		},
+export const {
+	createUser,
+	updateUser,
+	deleteUser,
+	createSession,
+	isAuthenticated,
+} = betterAuthComponent.createAuthFunctions<DataModel>({
+	// Must create a user and return the user id
+	onCreateUser: async (ctx, user) => {
+		return ctx.db.insert("users", {})
+	},
 
-		// Delete the user when they are deleted from Better Auth
-		onDeleteUser: async (ctx, userId) => {
-			await ctx.db.delete(userId as Id<"users">)
-		},
-	})
+	// Delete the user when they are deleted from Better Auth
+	onDeleteUser: async (ctx, userId) => {
+		await ctx.db.delete(userId as Id<"users">)
+	},
+})
 
 // Example function for getting the current user
 // Feel free to edit, omit, etc.
