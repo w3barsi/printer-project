9e9c969ed1d4b062f593df12ae4e82e93ad5ca91
w3barsi <darcybalaga@gmail.com>
2025-09-30 15:08:37 +0800

    style(ui): added logo to navbar home redirect

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -1,4 +1,3 @@
-import { Command } from "lucide-react";
 import * as React from "react";
 
 import { MainNavGroup } from "@/components/sidebar/group-nav-main";
@@ -26,8 +25,8 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <Link to="/">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <Command className="size-4" />
+                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg p-1">
+                  <img src="/logo-small.svg" alt="logo" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">Darcy Graphix</span>
