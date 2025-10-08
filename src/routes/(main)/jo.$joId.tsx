54cf8c8f848729428adc6949de721a1d01cb8fbc
w3barsi <darcybalaga@gmail.com>
2025-10-08 09:15:12 +0800

    style(jo): made delete description more descriptive

--- src/routes/(main)/jo.$joId.tsx
+++ src/routes/(main)/jo.$joId.tsx
@@ -291,8 +291,7 @@ function JobOrderCard() {
                 <AlertDialogHeader>
                   <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
                   <AlertDialogDescription>
-                    This action cannot be undone. This will permanently delete your
-                    account and remove your data from our servers.
+                    This action cannot be undone. This will permanently delete this Job Order.
                   </AlertDialogDescription>
                 </AlertDialogHeader>
                 <AlertDialogFooter>
