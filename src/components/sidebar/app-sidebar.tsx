177db791227081e928b8af7af0bd0586fa5a5cef
w3barsi <darcybalaga@gmail.com>
2025-10-15 18:56:48 +0800

    style(ui): made logo in sidebar always white bg

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -25,7 +25,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <Link to="/">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg p-1">
+                <div className="text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg bg-white p-1">
                   <img src="/logo-small.svg" alt="logo" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
