08662ceaa41855ed4e550822c7186445d8a44b80
w3barsi <darcybalaga@gmail.com>
2025-10-28 09:33:50 +0800

    fix(auth): try adding staletime of 1minute

--- src/lib/auth/queries.ts
+++ src/lib/auth/queries.ts
@@ -5,6 +5,7 @@ export const authQueryOptions = () =>
   queryOptions({
     queryKey: ["user"],
     queryFn: ({ signal }) => $fetchAuth({ signal }),
+    staleTime: 1 * 60,
   });
 
 export type AuthQueryResult = Awaited<ReturnType<typeof $fetchAuth>>;
