45c949ff8239f82eace219c2517d0ad9a9d6ef97
w3barsi <darcybalaga@gmail.com>
2025-11-07 02:24:23 +0800

    chore(convex): commented out automatic migrations temporarily

--- convex/migrations.ts
+++ convex/migrations.ts
@@ -1,10 +1,11 @@
 import { Migrations } from "@convex-dev/migrations";
 
-import { components, internal } from "./_generated/api.js";
+import { components } from "./_generated/api.js";
 import { DataModel } from "./_generated/dataModel.js";
 
 export const migrations = new Migrations<DataModel>(components.migrations);
-export const runMigrations = migrations.runner(internal.migrations.joOrderedOn);
+export const run = migrations.runner();
+// export const runMigrations = migrations.runner(internal.migrations.joOrderedOn);
 
 export const joOrderedOn = migrations.define({
   table: "jo",
