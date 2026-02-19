3fd47a2f6f443006648971b3d1b43369852068e5
w3barsi <darcybalaga@gmail.com>
2026-02-19 11:13:53 +0800

    feat: nested Container component for more control

--- src/components/layouts/container.tsx
+++ src/components/layouts/container.tsx
@@ -1,18 +1,23 @@
 import type { ComponentProps } from "react";
 
 import { cn } from "@/lib/utils";
+import type { ClassValue } from "clsx";
 
 /**
  * -------------------------------------------------------------------------------------
  * `container mx-auto p-2 md:p-4`.
  * -------------------------------------------------------------------------------------
  */
-export function Container({ children, className }: ComponentProps<"div">) {
+export function Container({
+  children,
+  className,
+  parentClassName,
+}: ComponentProps<"div"> & { parentClassName?: ClassValue }) {
   return (
-    <div
-      className={cn("container mx-auto max-w-xl gap-2 p-2 md:gap-4 md:p-4", className)}
-    >
-      {children}
+    <div className={cn("flex w-full justify-center", parentClassName)}>
+      <div className={cn("container max-w-xl gap-2 p-2 md:gap-4 md:p-4", className)}>
+        {children}
+      </div>
     </div>
   );
 }
